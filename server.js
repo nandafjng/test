823ca9d626589519200f95201cbed360d4024b47
nandafjng <142982240+nandafjng@users.noreply.github.com>
2025-05-23 06:26:52 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -49,8 +49,11 @@ const allowedOrigins = [
   'http://localhost:5500',
   'http://localhost:3000',
   'http://127.0.0.1:5500',
+  'http://127.0.0.1:5501',
   'https://rainbow-lokum-fc45f3.netlify.app', // Your new Netlify URL
   'https://bucolic-naiad-8de588.netlify.app', // Your old Netlify URL
+  'nandacobacoba.netlify.app',
+  'nandamasihnyoba.netlify.app'
 ];
 
 app.use(cors({
